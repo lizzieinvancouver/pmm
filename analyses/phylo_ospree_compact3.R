ee62b3d4fd33c3bf94be3454bcd6e2fa839be31d
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2021-07-05 08:21:21 -0700

    Adding back in some of my code that I missed

--- analyses/phylo_ospree_compact3.R
+++ analyses/phylo_ospree_compact3.R
@@ -2,10 +2,22 @@
 ## By Lizzie ## 
 ## Taken mainly from Phylo_ospree_reanalyses.R ##
 
+
+## housekeeping 
+rm(list=ls())
+options(stringsAsFactors = FALSE)
+
+# Setting working directory.
+# Add in your own path in an if statement for your file structure (please don't delete this)
+if(length(grep("Lizzie", getwd())>0)) {
+setwd("~/Documents/git/teaching/stan/pmm/analyses") } else if (length(grep("boomer", getwd()))>0) {setwd("boom/boom")
+}  else setwd("hereliesboomboom")
+
+# libraries
 library("ape")
 library("rstan")
 
-options(mc.cores = 4)
+options(mc.cores = parallel::detectCores())
 
 # Data
 phylo <- read.tree("input/ospreephylo.tre")
@@ -51,7 +63,7 @@ testme <- stan("stan/uber_threeslopeintercept.stan",
     iter=2000, chains=4,
     refresh = 10)
 
-saveRDS(testme, "compact3.rds")
+saveRDS(testme, "output/compact3.rds")
 
 ## ### Add species names
 ## names(testme)[4:(nspecies+3)] <- unique(d$phylo)
@@ -61,5 +73,37 @@ saveRDS(testme, "compact3.rds")
         
 ## ### Summarize lambda, b_zf, b_zc, intercepts, and sigmas
 ## summary(testme, pars = list("lam_interceptsa", "lam_interceptsb", "sigma_interceptsa", "sigma_interceptsbf", "sigma_interceptbc", "a_z", "b_zf", "b_zc" "sigma_y"))$summary
+
+
+# Reinstating some useful plotting code
+# See also https://github.com/lizzieinvancouver/pmm/blob/5014539f8a7cfc659298d20d49a0935a8ced305d/analyses/phlyo_opsree_compact.R
+fit <- readRDS("output/testme4.Rds")
+check_all_diagnostics(fit)
+fitsamples <- extract(fit)
+partition <- partition_div(fit)
+div_samples <- partition[[1]]
+nondiv_samples <- partition[[2]]
+
+c_dark <- c("#8F272780")
+green <- c("#00FF0080")
+
+partition <- partition_div(fit)
+div_params <- partition[[1]]
+nondiv_params <- partition[[2]]
+
+pars <- c("sigma_y", "lam_interceptsa", "sigma_interceptsa", "lam_interceptsbf", "sigma_interceptsbf",
+    "lam_interceptsbc", "sigma_interceptsbc", "lam_interceptsbp", "sigma_interceptsbp", "b_zf", "b_zc",
+    "b_zp", "a_z", "lp__")
+sumfit <- summary(fit)$summary
+sumfit[which(rownames(sumfit) %in% pars),]
+
+colz <- c(rep("#8F272780", nrow(nondiv_params[pars])), rep("#00FF0080", nrow(div_params[pars])))
+
+pdf(file="figures/ospreepairs.pdf")
+pairs(~sigma_y + lam_interceptsa + sigma_interceptsa + lam_interceptsbf +  sigma_interceptsbf + 
+    lam_interceptsbc +  sigma_interceptsbc +  lam_interceptsbp +  sigma_interceptsbp + 
+    b_zf + b_zc + b_zp + a_z + lp__, 
+    data=rbind(nondiv_params[pars], div_params[pars]), pch=21, cex=0.8, bg=colz)
+dev.off()
 
 
